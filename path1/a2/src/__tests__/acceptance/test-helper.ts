f96d93facd6860577689bd2097890680fe897c90
vishnupriya <vishnupriya.chirappa24@gmail.com>
2022-02-10 17:31:43 +0530

    Committed

---
+++ path1/a2/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {A2Application} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new A2Application({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: A2Application;
+  client: Client;
+}
